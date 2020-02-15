42cfe5a46f74a0586e24f8e67c9570d967ab91e7
Jeremy Stern <jzstern@gmail.com>
2020-02-15 10:50:27 -0700

    basic navbar styles

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <div id="app">
     <Navbar />
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/artist">Artist</router-link>
+    <div id="content">
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
 
@@ -28,16 +26,7 @@ export default {
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#content {
+  padding-top: 80px !important;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="navbar">
-    <p id="logo">Bound</p>
+    <router-link to="/">
+      <p id="logo">Bound ##</p>
+    </router-link>
+    <router-link to="/artist">Artist</router-link>
     <div id="wallet-address">
-      <p>wallet address</p>
+      <div id="green-dot"></div>
+      <p>0xc43r - 5gtd</p>
     </div>
   </div>
 </template>
@@ -16,15 +20,30 @@ export default {
 <style lang="scss">
 @import "../styles/global.scss";
 
+#green-dot {
+  width: 6px;
+  height: 6px;
+  border-radius: 50px;
+  margin-right: 5px;
+  background: linear-gradient(180deg, #398557 0%, #33c16b 100%);
+}
+
 #logo {
-  // position: absolute;
+  margin-left: 20px;
+  font-weight: bold;
 }
 
 #navbar {
   width: 100%;
+  height: 60px;
   position: absolute;
   display: flex;
   justify-content: space-between;
+  align-items: center;
+
+  &.router-link-exact-active {
+    color: #42b983;
+  }
 }
 
 #wallet-address {
@@ -32,10 +51,14 @@ export default {
   height: 34px;
   background: #e3e5e8;
   border-radius: 4px;
-  margin-right: 50px;
+  margin-top: 10px;
+  margin-right: 40px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   p {
-    @extend %center;
     color: #1d1f20;
     opacity: 0.6;
   }
