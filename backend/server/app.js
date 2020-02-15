f308bb4fa6a1ab91979d4e005186e4ad5df689e0
feewet <feewet@feewets-MacBook-Pro.local>
2020-02-15 15:12:35 -0700

    use splunk to watch events

--- backend/server/app.js
+++ backend/server/app.js
@@ -13,6 +13,7 @@ const infuraSecret = '3950f955052d425abf9327d3d298a91c'
 const infuraTestnetEndpoint = 'rinkeby.infura.io/v3/e6f6aa0719e64da1bc6d8be352171860'
 //const infuraMainnetEndpoint = 'mainnet.infura.io/v3/e6f6aa0719e64da1bc6d8be352171860'
 const githubAccessToken = 'b1fecd841b0240b90a41450f9b207ee9b66f0b1c'
+const ethLoggerAccessToken = '0d744933c6b0181de53a25009e59644c0e33f72d'
 
 // defining the Express app
 const app = express();
