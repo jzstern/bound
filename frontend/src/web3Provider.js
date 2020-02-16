76e9b523062b6480140a8160a9dd3052cac334d9
feewet <feewet@feewets-MacBook-Pro.local>
2020-02-16 04:04:52 -0700

    merge

--- frontend/src/web3Provider.js
+++ frontend/src/web3Provider.js
@@ -1,7 +1,6 @@
 import Fortmatic from 'fortmatic';
 import Web3 from "web3";
 
-<<<<<<< HEAD
 const customNodeOptions = {
     rpcUrl: 'http://127.0.0.1:7545',//, // your own node url
     chainId: 5777 // chainId of your own node
@@ -18,8 +17,6 @@ export const web3Provider = web3Fortmatic;
 export const fm = new Fortmatic("pk_test_F64FF8191AA41844");
 export const web3Provider = new Web3(fm.getProvider());
 **/
-=======
 export const fm = new Fortmatic("pk_test_F64FF8191AA41844");
 export const web3Provider = new Web3(fm.getProvider());
 // export const Box = require('3box');
->>>>>>> 1a31a32facc57148ac0aedb3d6be2fc9766366d5
