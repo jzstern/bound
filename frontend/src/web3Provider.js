ee902991bd1e111984f1481e7ee854391a24c094
Michael McKain <michael.mckain94@gmail.com>
2020-02-21 20:46:41 -0700

    commit all

--- frontend/src/web3Provider.js
+++ frontend/src/web3Provider.js
@@ -1,18 +1,17 @@
-import Fortmatic from 'fortmatic';
+import Fortmatic from "fortmatic";
 import Web3 from "web3";
 
 const customNodeOptions = {
-  rpcUrl: 'http://127.0.0.1:7545',//, // your own node url
+  rpcUrl: "http://127.0.0.1:7545", //, // your own node url
   chainId: 5777 // chainId of your own node
-}
+};
 
 /** Setting network to localhost blockchain **/
-export const fm = new Fortmatic('pk_test_F64FF8191AA41844', customNodeOptions);
-let web3Fortmatic = new Web3(fm.getProvider());
-window.web3 = web3Fortmatic;
-export const web3Provider = web3Fortmatic;
+// export const fm = new Fortmatic('pk_test_F64FF8191AA41844', customNodeOptions);
+// let web3Fortmatic = new Web3(fm.getProvider());
+// window.web3 = web3Fortmatic;
+// export const web3Provider = web3Fortmatic;
 
-/** production
+// production;
 export const fm = new Fortmatic("pk_test_F64FF8191AA41844");
 export const web3Provider = new Web3(fm.getProvider());
-**/
